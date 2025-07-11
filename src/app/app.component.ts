250e20dfc957c63e146e7d9594603b28ed15d041
FrankSua15 <frankosua@gmail.com>
2025-07-10 20:20:38 -0500

    Se termina de estructurar template completo del bento y se agregan funcionalidades

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLinkWithHref } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
-  title = 'bentoAngular';
+  irALink(url: string) {
+    window.open(url, '_blank'); // Abre en una nueva pestaña
+    // window.location.href = url; O usa esta línea para redirigir en la misma pestaña
+  }
 }
+
